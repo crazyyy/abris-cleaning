a9ef799f15c1c46c27893be345e2145be63ab3d1
crazyyy <crazyyy@gmail.com>
2015-08-24 15:36:12 +0300

    image rebuilds before

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,5 +15,106 @@
     }
 }
 }());
+// top navi
+jQuery(function($) {
+  $(document).ready( function() {
+    //enabling stickUp on the '.navbar-wrapper' class
+    $('header').stickUp({
+                  parts: {
+                    0:'home',
+                    1:'features',
+                    2: 'about',
+                    3: 'supers',
+                    4: 'review',
+                    5: 'contact'
+                  },
+                  itemClass: 'menuItem',
+                  itemHover: 'current'
+                });
+  });
+});
+// Плавное перемещение
+$('a[href^="#"]').click(function () {
+  elementClick = $(this).attr("href");
+  destination = $(elementClick).offset().top;
+  if($.browser.safari){
+    $('body').animate( { scrollTop: destination }, 800);
+  }else{
+    $('html').animate( { scrollTop: destination }, 800);
+    $('body').animate( { scrollTop: destination }, 800);
+  }
+  return false;
+});
+// Плавное перемещение
 
-// Place any jQuery/helper plugins in here.
+var day = new Date();
+timeto= new Date(day.getFullYear(), day.getMonth(), (day.getDate() + 1), 00, 00);
+$('.timer').countdown({until: timeto, format: 'HMS'});
+
+jQuery(function($){
+
+  var day = new Date();
+  timeto= new Date(day.getFullYear(), day.getMonth(), (day.getDate() + 1), 00, 00);
+  $('.timer').countdown({until: timeto, format: 'HMS'});
+
+
+  $("input[name='phone']").mask("+7 (999) 999-99-99");
+});
+
+
+// цифры
+$(document).ready(function() {
+  $('.scroll-animate').each(function () {
+    var block = $(this);
+    $(window).scroll(function() {
+      var top = block.offset().top;
+      var bottom = block.height()+top;
+      top = top - $(window).height();
+      var scroll_top = $(this).scrollTop();
+      if ((scroll_top > top) && (scroll_top < bottom)) {
+        if (!block.hasClass('animate')) {
+          block.addClass('animate');
+          block.trigger('animateIn');
+        }
+      } else {
+        block.removeClass('animate');
+        block.trigger('animateOut');
+      }
+    });
+  });
+
+  $('.block-second').on('animateIn', function() {
+      $(this).find('.num').each(function() {
+        var count =  parseInt($(this).attr('data-number'));
+        var block = $(this);
+        var timeout = null;
+        var step = 1;
+        timeout = setInterval(function() {
+          if (step == 25) {
+            block.text(count.toString());
+            clearInterval(timeout);
+          } else {
+            block.text((Math.floor(count*step/25)).toString());
+            step++;
+          }
+        }, 60);
+      });
+  });
+  new WOW().init();
+});
+
+// services
+$(document).ready(function(){
+  $('#s1').show();
+  $('.service-link:first').addClass('current');
+
+  $('li[data-service]').on('click', function() {
+    var sLocation = $(this).attr('data-service');
+    var service = $('#'+sLocation);
+    $('.service-link').removeClass('current');
+    $(this).addClass('current');
+    $('.service-block').hide();
+    service.show();
+  });
+});
+// services
